c8592360f258e61a911079cbb8789a8b5dd142ac
angelasalcido945 <angelasalcido945@gmail.com>
2025-10-28 20:31:48 +0000

    save

--- 3_control_flow/2_switch.js
+++ 3_control_flow/2_switch.js
@@ -7,7 +7,7 @@ if (course.toLowerCase() === 'cs 2500'){
     
 }else if(course.toLowerCase() === 'cs 3310')
     console.log("algorithm");
-}else if(course.toLowerCase() === 'cs 4310'){
+else if(course.toLowerCase() === 'cs 4310'){
     console.log("database");
 }else{
     console.log("other courses");
